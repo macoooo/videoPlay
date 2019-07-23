070f5c399c790396c5e359b155280f8140e10d8d
macoooo <86319668@qq.com>
2019-07-23 05:30:06 -0700

    Initial Commit

---
+++ VideoPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoPlayer
+//
+//  Created by 强淑婷 on 2019/7/23.
+//  Copyright © 2019 强淑婷. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
